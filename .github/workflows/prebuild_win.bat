c1ff5b30b5c729051837516d18988f25f6b5e397
Paul Yushkevich <pyushkevich@gmail.com>
2024-08-12 13:49:44 -0400

    try to build without using fetch

--- .github/workflows/prebuild_win.bat
+++ .github/workflows/prebuild_win.bat
@@ -3,7 +3,7 @@ setlocal enabledelayedexpansion
 
 REM Create build and install directories
 mkdir be\install
-REM cd be
+cd be
 
 REM Read the architecture parameter
 REM Check the first command-line parameter
@@ -13,19 +13,41 @@ if "%1" == "AMD64" (
     set ARCH=Win32
 )
 
-REM Run CMake to configure the project
+REM Build ITK
+git clone -b v5.4.0 https://github.com/InsightSoftwareConsortium/ITK.git ITK && ^
 cmake ^
-    -DFETCH_DEPENDENCIES=ON ^
-    -DDEPENDENCIES_ONLY=OFF ^
-    -DCMAKE_INSTALL_PREFIX=.\be\install ^
+    -DModule_MorphologicalContourInterpolation=ON ^
+    -DBUILD_EXAMPLES=OFF ^
+    -DBUILD_TESTING=OFF ^
     -DCMAKE_BUILD_TYPE=Release ^
-    -A %ARCH% ^
-    -B c:\prebuild ^
-    .
+    -DCMAKE_INSTALL_PREFIX=.\be\install ^
+    -B c:\prebuild\itk ^
+    ITK && ^
+cmake --build c:\prebuild\itk --config Release --target install -- -m
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
-REM Build the project using all available processors
-cmake --build c:\prebuild --config Release --target install -- -m
+REM Build VTK
+git clone -b v9.3.1 https://github.com/Kitware/VTK.git VTK && ^
+cmake ^
+    -DBUILD_EXAMPLES=OFF ^
+    -DBUILD_TESTING=OFF ^
+    -DBUILD_SHARED_LIBS=OFF ^
+    -DVTK_REQUIRED_OBJCXX_FLAGS="" ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_PREFIX=.\be\install ^
+    -B c:\prebuild\vtk ^
+    VTK && ^
+cmake --build c:\prebuild\vtk --config Release --target install -- -m
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
+REM Build Greedy
+git clone -b master https://github.com/pyushkevich/greedy.git greedy && ^
+cmake ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_PREFIX=.\be\install ^
+    -B c:\prebuild\greedy ^
+    greedy && ^
+cmake --build c:\prebuild\greedy --config Release --target install -- -m
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
 endlocal
