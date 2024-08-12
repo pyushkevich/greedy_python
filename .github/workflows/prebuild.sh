a36bd1e98e4939407073cdf6874023d0c6cf3033
Paul Yushkevich <pyushkevich@gmail.com>
2024-08-12 15:15:30 -0400

    again VTK crashed on build

--- .github/workflows/prebuild.sh
+++ .github/workflows/prebuild.sh
@@ -2,11 +2,40 @@
 set -x -e 
 
 mkdir -p be/install && cd be
+
+# Build ITK
+git clone -b v5.4.0 https://github.com/InsightSoftwareConsortium/ITK.git ITK
 cmake \
-    -DFETCH_DEPENDENCIES=ON \
-    -DDEPENDENCIES_ONLY=ON \
+    -DModule_MorphologicalContourInterpolation=ON \
+    -DBUILD_EXAMPLES=OFF \
+    -DBUILD_TESTING=OFF \
+    -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=./install \
     -DCMAKE_BUILD_TYPE=Release \
-    ..
+    -B ITK/build \
+    ITK
+cmake -B ITK/build --target install
 
-make && make install
+# Build VTK
+git clone -b v9.3.1 https://github.com/Kitware/VTK.git VTK
+cmake \
+    -DBUILD_EXAMPLES=OFF \
+    -DBUILD_TESTING=OFF \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_SHARED_LIBS=OFF \
+    -DVTK_REQUIRED_OBJCXX_FLAGS="" \
+    -DCMAKE_INSTALL_PREFIX=./install \
+    -DCMAKE_BUILD_TYPE=Release \
+    -B VTK/build \
+    VTK
+cmake -B VTK/build --target install
+
+# Build Greedy
+git clone -b master https://github.com/pyushkevich/greedy.git greedy
+cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=./install \
+    -DCMAKE_PREFIX_PATH=./install \
+    -B greedy/build \
+    greedy
+cmake -B greedy/build --target install
--- .github/workflows/prebuild_win.bat
+++ .github/workflows/prebuild_win.bat
@@ -13,6 +13,29 @@ if "%1" == "AMD64" (
     set ARCH=Win32
 )
 
+REM Build VTK
+git clone -b v9.3.1 https://github.com/Kitware/VTK.git VTK && ^
+cmake ^
+    -DBUILD_EXAMPLES=OFF ^
+    -DBUILD_TESTING=OFF ^
+    -DBUILD_SHARED_LIBS=OFF ^
+    -DVTK_MODULE_ENABLE_VTK_IOGeometry=YES ^
+    -DVTK_MODULE_ENABLE_VTK_IOPLY=YES ^
+    -DVTK_MODULE_ENABLE_VTK_IOLegacy=YES ^
+    -DVTK_MODULE_ENABLE_VTK_IOXML=YES ^
+    -DVTK_MODULE_ENABLE_VTK_CommonCore=YES ^
+    -DVTK_MODULE_ENABLE_VTK_CommonDataModel=YES ^
+    -DVTK_MODULE_ENABLE_VTK_CommonExecutionModel=YES ^
+    -DVTK_MODULE_ENABLE_VTK_FiltersCore=YES ^
+    -DVTK_BUILD_ALL_MODULES=OFF ^
+    -DVTK_REQUIRED_OBJCXX_FLAGS="" ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_PREFIX=.\be\install ^
+    -B c:\prebuild\vtk ^
+    VTK && ^
+cmake --build c:\prebuild\vtk --config Release --target install -- -m
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
 REM Build ITK
 git clone -b v5.4.0 https://github.com/InsightSoftwareConsortium/ITK.git ITK && ^
 cmake ^
@@ -26,24 +49,11 @@ cmake ^
 cmake --build c:\prebuild\itk --config Release --target install -- -m
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
-REM Build VTK
-git clone -b v9.3.1 https://github.com/Kitware/VTK.git VTK && ^
-cmake ^
-    -DBUILD_EXAMPLES=OFF ^
-    -DBUILD_TESTING=OFF ^
-    -DBUILD_SHARED_LIBS=OFF ^
-    -DVTK_REQUIRED_OBJCXX_FLAGS="" ^
-    -DCMAKE_BUILD_TYPE=Release ^
-    -DCMAKE_INSTALL_PREFIX=.\be\install ^
-    -B c:\prebuild\vtk ^
-    VTK && ^
-cmake --build c:\prebuild\vtk --config Release --target install -- -m
-if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
-
 REM Build Greedy
 git clone -b master https://github.com/pyushkevich/greedy.git greedy && ^
 cmake ^
     -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_PREFIX_PATH=.\be\install ^
     -DCMAKE_INSTALL_PREFIX=.\be\install ^
     -B c:\prebuild\greedy ^
     greedy && ^
