682b4e1c1b452e4db9e7aa5d304dba769971d5da
Paul Yushkevich <pyushkevich@gmail.com>
2024-08-13 13:11:14 -0400

    still working on it

--- .github/workflows/prebuild.sh
+++ .github/workflows/prebuild.sh
@@ -3,20 +3,6 @@ set -x -e
 
 mkdir -p be/install && cd be
 
-# Build ITK
-git clone -b v5.2.1 https://github.com/InsightSoftwareConsortium/ITK.git ITK
-cmake \
-    -DModule_MorphologicalContourInterpolation=ON \
-    -DBUILD_EXAMPLES=OFF \
-    -DBUILD_TESTING=OFF \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=./install \
-    -DCMAKE_BUILD_TYPE=Release \
-    -B ITK/build \
-    ITK
-
-cmake --build ITK/build --target install
-
 # Download and build VTK
 if [[ $RUNNER_OS == "Linux" && $RUNNER_ARCH == "X64" ]]; then
   VTK_BINARY=vtk-wheel-sdk-9.3.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.tar.xz
@@ -33,8 +19,21 @@ fi
 # Install VTK from binary wheels provided by Kitware
 mkdir -p install/vtk
 curl -L https://www.vtk.org/files/release/9.3/${VTK_BINARY} -o ./be/install/vtk/vtk-wheel-sdk.tar.xz
-tar -xJvf ./be/install/vtk/vtk-wheel-sdk.tar.xz -C $PWD/be/install/vtk
-VTKDIR=$(dirname $(find $PWD/install/vtk -name vtk-config.cmake))
+tar -xJvf ./be/install/vtk/vtk-wheel-sdk.tar.xz --strip-components 1 -C $PWD/be/install/vtk
+
+# Build ITK
+git clone -b v5.2.1 https://github.com/InsightSoftwareConsortium/ITK.git ITK
+cmake \
+    -DModule_MorphologicalContourInterpolation=ON \
+    -DBUILD_EXAMPLES=OFF \
+    -DBUILD_TESTING=OFF \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=./install \
+    -DCMAKE_BUILD_TYPE=Release \
+    -B ITK/build \
+    ITK
+
+cmake --build ITK/build --target install
 
 #git clone -b v9.3.1 https://github.com/Kitware/VTK.git VTK
 #cmake \
@@ -55,7 +54,7 @@ cmake \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=./install \
     -DCMAKE_PREFIX_PATH="$PWD/install" \
-    -DVTK_DIR=$VTKDIR \
+    -DVTK_DIR=$PWD/install/vtk/vtk-9.3.1.data/headers/cmake \
     -B greedy/build \
     greedy
 
