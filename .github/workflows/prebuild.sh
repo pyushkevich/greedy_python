e98bb4ede781bb5c5fbb50828ecdefc5d6351f9f
Paul Yushkevich <pyushkevich@gmail.com>
2024-08-13 13:02:20 -0400

    can we do this without windows?

--- .github/workflows/prebuild.sh
+++ .github/workflows/prebuild.sh
@@ -17,7 +17,23 @@ cmake \
 
 cmake --build ITK/build --target install
 
-# Find VTK header file
+# Download and build VTK
+if [[ $RUNNER_OS == "Linux" && $RUNNER_ARCH == "X64" ]]; then
+  VTK_BINARY=vtk-wheel-sdk-9.3.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.tar.xz
+elif [[ $RUNNER_OS == "macOS" && $RUNNER_ARCH == "X64" ]]; then
+  VTK_BINARY=vtk-wheel-sdk-9.3.1-cp310-cp310-macosx_10_10_x86_64.tar.xz
+elif [[ $RUNNER_OS == "macOS" && $RUNNER_ARCH == "ARM64" ]]; then
+  VTK_BINARY=vtk-wheel-sdk-9.3.1-cp310-cp310-macosx_11_0_arm64.tar.xz
+elif [[ $RUNNER_OS == "Windows" && $RUNNER_ARCH == "ARM64" ]]; then
+    VTK_BINARY=vtk-wheel-sdk-9.3.1-cp310-cp310-win_amd64.tar.xz
+else
+  exit 255
+fi
+
+# Install VTK from binary wheels provided by Kitware
+mkdir -p install/vtk
+curl -L https://www.vtk.org/files/release/9.3/${VTK_BINARY} -o ./be/install/vtk/vtk-wheel-sdk.tar.xz
+tar -xJvf ./be/install/vtk/vtk-wheel-sdk.tar.xz -C $PWD/be/install/vtk
 VTKDIR=$(dirname $(find $PWD/install/vtk -name vtk-config.cmake))
 
 #git clone -b v9.3.1 https://github.com/Kitware/VTK.git VTK
