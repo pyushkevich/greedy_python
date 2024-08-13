4b5bd1cc487e8dc60f71ea5c58e226615a671a1d
Paul Yushkevich <pyushkevich@gmail.com>
2024-08-13 10:37:35 -0400

    still trying 2

--- .github/workflows/prebuild.sh
+++ .github/workflows/prebuild.sh
@@ -14,7 +14,6 @@ cmake \
     -DCMAKE_BUILD_TYPE=Release \
     -B ITK/build \
     ITK
-cmake --build ITK/build --target install
 
 # Build VTK
 #git clone -b v9.3.1 https://github.com/Kitware/VTK.git VTK
@@ -35,7 +34,9 @@ git clone -b master https://github.com/pyushkevich/greedy.git greedy
 cmake \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=./install \
-    -DCMAKE_PREFIX_PATH=./install;./install/vtk/vtk-9.3.1.data/headers/cmake \
+    -DCMAKE_PREFIX_PATH=./install:./install/vtk/vtk-9.3.1.data/headers/cmake \
     -B greedy/build \
     greedy
+
+cmake --build ITK/build --target install
 cmake --build greedy/build --target install
