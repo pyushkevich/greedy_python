5c3d74b92460e85253f1d485c0ea4cdcceaf93cf
Paul Yushkevich <pyushkevich@gmail.com>
2024-08-13 17:26:18 -0400

    last step, I think

--- .github/workflows/prebuild.sh
+++ .github/workflows/prebuild.sh
@@ -6,20 +6,29 @@ mkdir -p be/install && cd be
 # Download and build VTK
 if [[ $1 =~ ubuntu-.* ]]; then
   VTK_BINARY=vtk-wheel-sdk-9.3.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.tar.xz
+  DYLD_SUFFIX=so
+  MAKEFLAGS="-- -j 8"
 elif [[ $1 == macos-13 ]]; then
   VTK_BINARY=vtk-wheel-sdk-9.3.1-cp310-cp310-macosx_10_10_x86_64.tar.xz
+  DYLD_SUFFIX=dylib
+  MAKEFLAGS="-- -j 8"
 elif [[ $1 == macos-14 ]]; then
   VTK_BINARY=vtk-wheel-sdk-9.3.1-cp310-cp310-macosx_11_0_arm64.tar.xz
+  DYLD_SUFFIX=dylib
 elif [[ $1 =~ windows-.* ]]; then
   VTK_BINARY=vtk-wheel-sdk-9.3.1-cp310-cp310-win_amd64.tar.xz
+  DYLD_SUFFIX=dll
 else
   exit 255
 fi
 
 # Install VTK from binary wheels provided by Kitware
-mkdir -p install/vtk
+mkdir -p install/vtk install/vtk/shared
 curl -L https://www.vtk.org/files/release/9.3/${VTK_BINARY} -o ./install/vtk/vtk-wheel-sdk.tar.xz
 tar -xJvf ./install/vtk/vtk-wheel-sdk.tar.xz --strip-components 1 -C $PWD/install/vtk
+ln $(find $PWD/install/vtk/build -name "*.${DYLD_SUFFIX}") install/vtk/shared
+
+# Link the shared libraries needed for delocate into a simple directory
 
 # Build ITK
 git clone -b v5.2.1 https://github.com/InsightSoftwareConsortium/ITK.git ITK
@@ -33,7 +42,7 @@ cmake \
     -B ITK/build \
     ITK
 
-cmake --build ITK/build --target install
+cmake --build ITK/build --target install $MAKEFLAGS
 
 #git clone -b v9.3.1 https://github.com/Kitware/VTK.git VTK
 #cmake \
@@ -58,4 +67,4 @@ cmake \
     -B greedy/build \
     greedy
 
-cmake --build greedy/build --target install
+cmake --build greedy/build --target install $MAKEFLAGS
