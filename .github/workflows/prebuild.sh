c9061efbdc408a29c1ee9c68d164542b8215714e
Paul Yushkevich <pyushkevich@gmail.com>
2024-08-13 10:18:50 -0400

    still trying

--- .github/workflows/prebuild.sh
+++ .github/workflows/prebuild.sh
@@ -14,28 +14,28 @@ cmake \
     -DCMAKE_BUILD_TYPE=Release \
     -B ITK/build \
     ITK
-cmake -B ITK/build --target install
+cmake --build ITK/build --target install
 
 # Build VTK
-git clone -b v9.3.1 https://github.com/Kitware/VTK.git VTK
-cmake \
-    -DBUILD_EXAMPLES=OFF \
-    -DBUILD_TESTING=OFF \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DBUILD_SHARED_LIBS=OFF \
-    -DVTK_REQUIRED_OBJCXX_FLAGS="" \
-    -DCMAKE_INSTALL_PREFIX=./install \
-    -DCMAKE_BUILD_TYPE=Release \
-    -B VTK/build \
-    VTK
-cmake -B VTK/build --target install
+#git clone -b v9.3.1 https://github.com/Kitware/VTK.git VTK
+#cmake \
+#    -DBUILD_EXAMPLES=OFF \
+#    -DBUILD_TESTING=OFF \
+#    -DCMAKE_BUILD_TYPE=Release \
+#    -DBUILD_SHARED_LIBS=OFF \
+#    -DVTK_REQUIRED_OBJCXX_FLAGS="" \
+#    -DCMAKE_INSTALL_PREFIX=./install \
+#    -DCMAKE_BUILD_TYPE=Release \
+#    -B VTK/build \
+#    VTK
+#cmake --build VTK/build --target install
 
 # Build Greedy
 git clone -b master https://github.com/pyushkevich/greedy.git greedy
 cmake \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=./install \
-    -DCMAKE_PREFIX_PATH=./install \
+    -DCMAKE_PREFIX_PATH=./install;./install/vtk/vtk-9.3.1.data/headers/cmake \
     -B greedy/build \
     greedy
-cmake -B greedy/build --target install
+cmake --build greedy/build --target install
--- .github/workflows/prebuild_win.bat
+++ .github/workflows/prebuild_win.bat
@@ -54,7 +54,7 @@ REM Build Greedy
 git clone -b master https://github.com/pyushkevich/greedy.git greedy && ^
 cmake ^
     -DCMAKE_BUILD_TYPE=Release ^
-    -DCMAKE_PREFIX_PATH=.\be\install;.\be\install\vtk\vtk-9.3.1.data\headers\cmake\vtk-config.cmake ^
+    -DCMAKE_PREFIX_PATH=.\install;.\install\vtk\vtk-9.3.1.data\headers\cmake ^
     -DCMAKE_INSTALL_PREFIX=.\be\install ^
     -B c:\prebuild\greedy ^
     greedy && ^
